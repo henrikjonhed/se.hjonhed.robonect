1ddbcd495efc581c2b0e71351759a85a41576462
Henrik Jonhed <henrik.jonhed@soundtrackyourbrand.com>
2023-05-14 15:24:49 +0200

    Bump version to sentry

--- app.ts
+++ app.ts
@@ -2,7 +2,7 @@ import Homey from "homey";
 import { Log } from 'homey-log';
 
 class RobonectApp extends Homey.App {
-  logger: typeof Log = new Log({ homey: this.homey, options: {release: '1.0.6'} })
+  logger: typeof Log = new Log({ homey: this.homey, options: {release: '1.0.7'} })
 
   async setupLogging() {
     // @ts-ignore
